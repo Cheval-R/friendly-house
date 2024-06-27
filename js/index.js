1bb5e628d46f3f31a121c79e3cb47abce688112b
Cheval-R <rerisitdikov@gmail.com>
2024-06-27 14:48:06 +0300

    pages

--- js/index.js
+++ js/index.js
@@ -13289,23 +13289,29 @@ async function initMap() {
 // This entry need to be wrapped in an IIFE because it need to be isolated against other entry modules.
 (() => {
 // ! SLIDER
-if (document.documentElement.clientWidth < 800) {
-  $('.pets__content').slick({
-    centerMode: true,
-    variableWidth: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true
-  });
 
-  // Добавляем атрибут aria-label
-  let arrowPrev = $('.slick-prev');
-  arrowPrev.attr('aria-label', 'Предыдущий слайд');
-  let arrowNext = $('.slick-next');
-  arrowNext.attr('aria-label', 'Следующий слайд');
-}
+// ! SLIDER
+$(window).resize(function () {
+  if (document.documentElement.clientWidth <= 800) {
+    $('.pets__content').slick({
+      centerMode: true,
+      variableWidth: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: true
+    });
+
+    // Добавляем атрибут aria-label
+    let arrowPrev = $('.slick-prev');
+    arrowPrev.attr('aria-label', 'Предыдущий слайд');
+    let arrowNext = $('.slick-next');
+    arrowNext.attr('aria-label', 'Следующий слайд');
+  } else if ($('.pets__content').hasClass('slick-initialized')) {
+    $('.pets__content').slick('unslick');
+  }
+});
 })();
 
 /******/ })()
